4f7887b9b2fb4366add0c216172608234a7c6d01
mbullin2 <michaelbullinger4@gmail.com>
2017-09-16 20:17:26 -0600

    Got rid of unneeded code, modified my code and added another test file

--- Lab011.cpp
+++ Lab011.cpp
@@ -1,5 +1,4 @@
 
-#include "stdafx.h"
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -7,25 +6,27 @@ using namespace std;
 
 int main()
 {
-	//fstream file;
+	// Variables that store the file name, a temporary variable to read the file,
+	// the length of the file and store the first two and last two numbers of a file.
 	string fileName;
-	int i;
-	int length = 0;
-	int numberOne = 0;
-	int numberTwo = 0;
-	int numberThree = 0;
-	int numberFour = 0;
-	int temp = 0;
-
-	cout << "Please enter a filename ";
+	float i;
+	float length = 0;
+	float numberOne = 0;
+	float numberTwo = 0;
+	float numberThree = 0;
+	float numberFour = 0;
+	// Prompt the user for a file name and then try and open the file
+	cout << "This program counts the length of a file along with printing the first 2 and last 2 numbers," << endl;
+	cout << "Please enter a file name ";
 	getline(cin, fileName);
-	//file.open(fileName);
 	ifstream file(fileName);
+	// Check to see if the file opened, if not, print and error message and terminate the program
 	if (file.fail())
 	{
 		cerr << "Unable to open file" << endl;
 		return 1;
 	}
+	// Read from the file while keeping track of the length along with the first two and last two numbers
 	while (file >> i)
 	{
 		length++;
@@ -45,10 +46,10 @@ int main()
 		}
 		numberFour = i;
 	}
-	
-	cout << endl;
+	// Close the file
 	file.close();
-	cout << "The length of the file is " << length << endl;
+	// Print the results of the program
+	cout << endl << "The length of the file is " << length << endl;
 	cout << "The first number is " << numberOne << endl;
 	cout << "The second number is " << numberTwo << endl;
 	cout << "The second to last number is " << numberThree << endl;
